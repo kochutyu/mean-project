d5f2fb6fa7e9a9197728d66f5b238e4b3aad1a79
kochutyu <yura.kochut@israelit.pro>
2021-11-16 04:26:13 +0200

    Added > module [analytics | auth middleware]

--- routes/analytics.js
+++ routes/analytics.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const controller = require('../controllers/analytics');
+const passport = require('passport');
 const router = express.Router();
 
-router.get('/overview', controller.overview);
-router.get('/analytics', controller.analytics);
+router.get('/overview', passport.authenticate('jwt', {session: false}), controller.overview);
+router.get('/analytics', passport.authenticate('jwt', {session: false}), controller.analytics);
 
 module.exports = router;
