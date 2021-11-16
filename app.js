d3f20a33669c7c2725ea4a40cba30fb24cf9de04
kochutyu <yura.kochut@israelit.pro>
2021-11-16 05:18:51 +0200

    Updated > app [routes]

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(require('cors')(''))
 
 app.use('/api/auth', authRoutes);
 app.use('/api/analytics', analyticsRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/position', positionRoutes);
+app.use('/api/categories', categoryRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/positions', positionRoutes);
 
 module.exports = app;
